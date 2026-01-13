837c366fc85751d4dd01cd070fdf07294a366201
4gcnny5cxp-dot <4gcnny5cxp@privaterelay.appleid.com>
2026-01-13 22:28:05 +0100

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <header className="topbar">
+        <div className="topbarInner">
+          <a className="brand" href="/">
+            VitaWoon <span className="pill">wonen centraal</span>
+          </a>
+
+          <nav className="nav">
+            <a href="/woningen">Woningen</a>
+            <a href="/woningen/kaart">Kaart</a>
+            <a href="/vitacheck">VitaCheck</a>
+            <a href="/voor-naasten">Voor naasten</a>
+          </nav>
+        </div>
+      </header>
+
+      <Component {...pageProps} />
+
+      <footer className="footer">
+        <div className="footerInner">
+          <div>Demo • VitaWoon / VitaDomus</div>
+          <div>Hulp, ondersteuning en zorg optioneel</div>
+        </div>
+      </footer>
+    </>
+  );
+}
